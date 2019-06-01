b57983d2eeeadf4b2f0009b4c252c6248192bbfb
c4424 <c4424@live.com>
2019-06-01 10:43:52 -0700

    new

--- server.js
+++ server.js
@@ -1,64 +1,29 @@
-// BEGIN Boilerplate
-const express = require("express");
-const app = express();
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require('express')
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-const PORT = process.env.PORT || 8080;
+// Sets up the Express App
+// =============================================================
+var app = express()
+var PORT = process.env.PORT || 8080
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-const exphbs = require("express-handlebars");
+// Static directory
+app.use(express.static('public'))
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// Routes
+// =============================================================
+require('./routes/api-routes.js')(app)
 
-const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "devUser",
-  password: "",
-  database: "task_saver_db",
-  debug: ['ComQueryPacket', 'RowDataPacket']
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-// END Boilerplate
-
-// Root get route
-app.get("/", function(req, res) {
-  connection.query("SELECT * FROM tasks;", function(err, data) {
-    if (err) throw err;
-
-    res.render("index", { tasks: data });
-  });
-});
-
-// Post route -> back to home
-app.post("/", function(req, res) {
-
-  // When using the MySQL package, we'd use ?s in place of any values to be inserted, which are then swapped out with corresponding elements in the array
-  // This helps us avoid an exploit known as SQL injection which we'd be open to if we used string concatenation
-  // https://en.wikipedia.org/wiki/SQL_injection
-  connection.query("INSERT INTO tasks (task) VALUES (?)", [req.body.task], function(err, result) {
-    if (err) throw err;
-
-    res.redirect("/");
-  });
-});
-
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+// Starting our Express app
+// =============================================================
+app.listen(PORT, function () {
+  console.log('App listening on PORT ' + PORT)
+})
